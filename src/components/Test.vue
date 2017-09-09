b3093c6d49533faea2487fdac9da002763386ec2
ak1103dev <ak.apichan1103@gmail.com>
2017-09-09 19:11:16 +0700

    first commit

---
+++ src/components/Test.vue
@@ -0,0 +1,63 @@
+<template>
+  <q-layout>
+    <div id="test" class="layout-view">
+      <p>Welcome to your Vue.js app!</p>
+      <button class="primary" @click="checkSignin">check sign in</button>
+      <button class="primary" @click="signOut">sign out</button>
+      <input v-model="text">
+      <dropzone
+        id="myVueDropzone"
+        url="https://httpbin.org/post"
+        v-on:vdropzone-success="showSuccess"
+      >
+          <input type="hidden" name="token" value="xxx">
+      </dropzone>
+      <!-- <dropzone
+        id="uploadImage"
+        url="https://www.googleapis.com/upload/drive/v3/files?uploadType=media"
+        :thumbnailHeight="200"
+        :thumbnailWidth="200"
+      ></dropzone> -->
+      <p>xxxx</p>
+    </div>
+  </q-layout>
+</template>
+
+<script>
+import Dropzone from 'vue2-dropzone'
+import {
+  handleClientLoad,
+  handleAuthClick,
+  handleSignoutClick
+} from 'src/services/gapi'
+
+export default {
+  components: {
+    Dropzone
+  },
+  data () {
+    return {
+      text: ''
+    }
+  },
+  mounted () {
+    handleClientLoad()
+  },
+  methods: {
+    showSuccess (file) {
+      console.log('A file was successfully uploaded')
+      console.log('file', file)
+    },
+    checkSignin () {
+      const isSignedIn = gapi.auth2.getAuthInstance().isSignedIn.get()
+      console.log('is sign in', isSignedIn)
+      if (!isSignedIn) {
+        handleAuthClick()
+      }
+    },
+    signOut () {
+      handleSignoutClick()
+    }
+  }
+}
+</script>
---
+++ src/services/gapi.js
@@ -0,0 +1,67 @@
+const CLIENT_ID = '904089928675-bhn16r2l298guobqrveh4qp6ps6di74p.apps.googleusercontent.com'
+const API_KEY = 'AIzaSyAR52n9dgkDg2P99oWQfq7S8oCarFYy8Vs'
+const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest']
+const SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly'
+
+const handleAuthClick = (event) => {
+  gapi.auth2.getAuthInstance().signIn()
+}
+
+const handleSignoutClick = (event) => {
+  gapi.auth2.getAuthInstance().signOut()
+}
+
+const listFiles = () => {
+  gapi.client.drive.files.list({
+    'pageSize': 10,
+    'fields': 'nextPageToken, files(id, name)'
+  }).then(function (response) {
+    console.log('Files:')
+    var files = response.result.files
+    if (files && files.length > 0) {
+      for (var i = 0; i < files.length; i++) {
+        var file = files[i]
+        console.log(file.name + ' (' + file.id + ')')
+      }
+    }
+    else {
+      console.log('No files found.')
+    }
+  })
+}
+
+const updateSigninStatus = (isSignedIn) => {
+  console.log('is sign in', isSignedIn)
+  if (isSignedIn) {
+    // authorizeButton.style.display = 'none';
+    // signoutButton.style.display = 'block';
+    listFiles()
+  }
+  else {
+    // authorizeButton.style.display = 'block';
+    // signoutButton.style.display = 'none';
+  }
+}
+
+const initClient = () => {
+  gapi.client.init({
+    apiKey: API_KEY,
+    clientId: CLIENT_ID,
+    discoveryDocs: DISCOVERY_DOCS,
+    scope: SCOPES
+  }).then(function () {
+    const isSignedIn = gapi.auth2.getAuthInstance().isSignedIn.get()
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus)
+    updateSigninStatus(isSignedIn)
+  })
+}
+
+const handleClientLoad = () => {
+  gapi.load('client:auth2', initClient)
+}
+
+export {
+  handleClientLoad,
+  handleAuthClick,
+  handleSignoutClick
+}
